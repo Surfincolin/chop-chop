0a393e2f30fa34b598fd536b05763bff4a5de78e
Surfincolin <colinwageman@gmail.com>
2018-12-31 08:13:37 -0800

    Created track styles

--- src/js/components/Timeline.vue
+++ src/js/components/Timeline.vue
@@ -3,22 +3,81 @@
 	<div id="timeline" class="highlight-bc">
 		<div class="label row">Timeline</div>
 		<!-- <div id="thumbnail-track" class="track"></div> -->
+		<div id="tracks" class="row">
+			<div v-for="(t, key) in tracks" :key="key" class="track" >
+				<div class="header highlight-bc">
+					<p><strong>{{t.title}}</strong></p>
+				</div>
+				<div class="content gray-bg"></div>
+			</div>
+		</div>
 	</div>
 
 </template>
 
+<script>
+export default {
+	data() {
+	  return {
+	  	tracks: [{title: "v1", content: []}]
+	  }
+	},
+	methods:{
+
+	},
+	computed:{
+	  
+	},
+	created(){
+	  
+	},
+	mounted(){
+	 
+	},
+	beforeDestroy(){
+	  
+	},
+	watch:{
+	}
+}
+</script>
+
 <style lang="scss">
 	#timeline {
 		display: flex;
+		flex-direction: column;
 		border-top-width: 1px;
 		border-top-style: solid;
 		padding: 4px 20px;
+
+		#tracks {
+			width: 100%;
+		}
 
 		.track {
 			width: 100%;
 			height: 50px;
 			position: relative;
-			background: orange;
+			display: flex;
+
+			.header {
+				flex: 0 1 auto;
+				height: 100%;
+				width: 40px;
+				display: flex;
+				border-width: 1px;
+				border-style: solid;
+
+				p {
+					text-align: center;
+					margin: auto;
+				}
+			}
+
+			.content {
+				flex: 1 1 auto;
+				height: 100%;
+			}
 		}
 	}
 </style>
